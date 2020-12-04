7a137f45c746cf72831ac73e4333d13c880dbe0e
Sean Park <sean.inomire@gmail.com>
2020-12-04 09:47:49 -0800

    add deleteMovie mutation

--- graphql/db.js
+++ graphql/db.js
@@ -19,9 +19,9 @@ export const getById = (id) => {
 };
 
 export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
   if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
+    movies = cleanedMovies;
     return true;
   } else {
     return false;
@@ -30,7 +30,7 @@ export const deleteMovie = (id) => {
 
 export const addMovie = (name, score) => {
   const newMovie = {
-    id: `${movies.length - 1}`,
+    id: movies.length,
     name,
     score,
   };
