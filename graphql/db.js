bd58f1c2a7dd1506f16d34e20c29c30aa8c03b3a
Sean Park <sean.inomire@gmail.com>
2020-12-10 23:20:34 -0800

    add suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -1,61 +1,45 @@
-import fetch from "node-fetch";
-const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+import axios from "axios";
+const BASE_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
-  if (limit > 0) {
-    REQUEST_URL += `limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
-  return fetch(REQUEST_URL)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
 };
 
-export const getMovie = (id) => {
-  const request_url = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
-  return fetch(request_url)
-    .then((res) => res.json())
-    .then((json) => json.data.movie);
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
 };
-// let movies = [
-//   {
-//     id: 0,
-//     name: "Star Wars",
-//     score: 1,
-//   },
-//   {
-//     id: 1,
-//     name: "Avengers",
-//     score: 8,
-//   },
-// ];
 
-// export const getMovies = () => movies;
-
-// export const getById = (id) => {
-//   const filteredMovies = movies.filter((movie) => movie.id === id);
-//   return filteredMovies[0];
-// };
-
-// export const deleteMovie = (id) => {
-//   const cleanedMovies = movies.filter((movie) => movie.id !== id);
-//   if (movies.length > cleanedMovies.length) {
-//     movies = cleanedMovies;
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// export const addMovie = (name, score) => {
-//   const newMovie = {
-//     id: movies.length,
-//     name,
-//     score,
-//   };
-//   movies.push(newMovie);
-//   return newMovie;
-// };
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
+};
